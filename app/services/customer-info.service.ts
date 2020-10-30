f84bfc89e95bdaab5a242c4393a1a393c536dd72
Nuttaponn <nuttapon.nintao@gmail.com>
2020-10-30 20:16:42 +0700

    1 file modified

--- app/services/customer-info.service.ts
+++ app/services/customer-info.service.ts
@@ -1,27 +1,38 @@
 import { Injectable } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { CustomerInfo } from "../interface/customer-info.interface";
 
 @Injectable()
 export class CustomerInfoService {
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder) { }
 
-  getCustomerInfoTab(): FormGroup {
-    return this.fb.group({
-      customerProfile: this.getCustomerProfile(),
-      addressInfomation: this.getAddressInfomation()
-    });
+  getCustomerInfoTab(custProfile?: CustomerInfo): FormGroup {
+    if (custProfile) {
+      return this.fb.group({
+        customerProfile: this.getCustomerProfile(custProfile?.customerProfile),
+        addressInfomation: this.getAddressInfomation(custProfile?.addressInfomation)
+      });
+    } else {
+      return this.fb.group({
+        customerProfile: this.getCustomerProfile(),
+        addressInfomation: this.getAddressInfomation()
+      });
+    }
   }
 
-  getCustomerProfile(): FormGroup {
-    return this.fb.group({
-      identNumber: ["", [Validators.required]],
-      registContry: ["", [Validators.required]],
-      registDate: ["", [Validators.required]],
-      titleTh: ["", [Validators.required]],
-      nameTh: ["", [Validators.required]],
-      titleEn: ["", [Validators.required]],
-      nameEn: ["", [Validators.required]]
-    });
+  getCustomerProfile(custProfile?: CustomerInfo): FormGroup {
+    if (custProfile) {
+    } else {
+      return this.fb.group({
+        identNumber: ["", [Validators.required]],
+        registContry: ["", [Validators.required]],
+        registDate: ["", [Validators.required]],
+        titleTh: ["", [Validators.required]],
+        nameTh: ["", [Validators.required]],
+        titleEn: ["", [Validators.required]],
+        nameEn: ["", [Validators.required]]
+      });
+    }
   }
 
   getAddressInfomation(): FormGroup {
